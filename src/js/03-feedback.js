d485b7756effee68dec1abda71de05ff48e951df
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-10-27 13:38:28 +0300

    03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+const { throttle } = require('lodash');
 
+const formRef = document.querySelector('.feedback-form');
+
+loadData(formRef);
+
+formRef.addEventListener('input', throttle(onInputData, 500));
+formRef.addEventListener('submit', onSubmitData);
+
+function loadData(form) {
+  let feedback = localStorage.getItem('feedback-form-state');
+  if (feedback) {
+    feedback = JSON.parse(feedback);
+    [...form.elements].forEach(elem => {
+      if ((elem.nodeName === 'INPUT' || elem.nodeName === 'TEXTAREA') && feedback[elem.name]) {
+        elem.value = feedback[elem.name];
+      }
+    });
+  }
+}
+
+function saveData(target) {
+  if (target) {
+    const data = {};
+    [...target.elements].forEach(elem => {
+      if (elem.nodeName === 'INPUT' || elem.nodeName === 'TEXTAREA') {
+        data[elem.attributes.name.value] = elem.value;
+      }
+    });
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  }
+}
+
+function onInputData(e) {
+  if (e.currentTarget) {
+    saveData(e.currentTarget);
+  }
+}
+
+function onSubmitData(e) {
+  e.preventDefault();
+  for (const elem of [...e.currentTarget.elements]) {
+    if ((elem.nodeName === 'INPUT' || elem.nodeName === 'TEXTAREA') && elem.value === '') {
+      alert('Error!!! Empty fields!');
+      return;
+    }
+  }
+
+  saveData(e.currentTarget);
+  e.currentTarget.reset();
+  const data = localStorage.getItem('feedback-form-state');
+  console.log(data);
+}
