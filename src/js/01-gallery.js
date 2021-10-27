1706350a19811721a17203d7d1bee001cc07bdf7
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-10-27 12:05:22 +0300

    01 aand 02 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+galleryRef.innerHTML = createMarkup();
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+
+function createMarkup() {
+  let markup = '';
+  for (const { preview, original, description } of galleryItems) {
+    markup += `
+    <a class="gallery__item"  href="${original}"> 
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}>" />
+    </a>`;
+  }
+
+  return markup;
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+let currentTime;
+try {
+  currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+} catch (error) {
+  currentTime = 0;
+}
+
+let iframe = document.querySelector('iframe');
+let player = new Player(iframe);
+
+player
+  .setCurrentTime(Number(currentTime))
+  .then(function (seconds) {
+    console.log(`ok - ${seconds}`);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        currentTime = 0;
+        break;
+
+      default:
+        console.log(error);
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000),
+);
